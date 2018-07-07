dba9160b58f1e49f05c385d352e63e0ad87fa00b
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-07-07 17:01:50 +0000

    automatically backed up by learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -10,9 +10,8 @@ class CreateStudents < ActiveRecord::Migration[5.1]
   def change
    create_table :artists do |t|
      t.string :name
-     t.string :genre
-     t.integer :age
-     t.string :hometown
+     t.string :grade
+     t.string :birthdate
    end
  end
 
