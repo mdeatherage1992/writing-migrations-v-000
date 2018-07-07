87a01f07b72f9b48c86953392795f4645f5d50d3
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-07-07 17:03:06 +0000

    automatically backed up by learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -10,8 +10,6 @@ class CreateStudents < ActiveRecord::Migration[5.1]
   def change
    create_table :students do |t|
      t.string :name
-     t.string :grade
-     t.string :birthdate
    end
  end
 
